64aaba21663408d295286e1f405bccb952be9661
Jonas Hagenberg <38041374+jonas-hag@users.noreply.github.com>
2021-01-02 00:10:56 +0100

    - remove some code/function not needed anmyore (was for debugging) - function to evaluate the user input - set up main app structure

--- tonetrainer/__main__.py
+++ tonetrainer/__main__.py
@@ -2,7 +2,6 @@ import sys
 import sqlite3
 import requests
 import vlc
-import time
 
 
 class TonePair:
@@ -29,6 +28,10 @@ class TonePair:
             sql_query = "SELECT * FROM toneinfo WHERE forvo_available <> 'no' " \
                         "ORDER BY Random() LIMIT 1;"
             self.all_data = self.cursor.execute(sql_query).fetchone()
+            # all_data contains a tuple with the following fields:
+            # traditional, simplified, pinyin_1, pinyin_2, tone_1, tone_2,
+            # forvo_available, number_tested
+
             # query the word in forvo to get the link for the audio
             # 1. define the parameters
             payload = {"key": self.api_key,
@@ -46,6 +49,7 @@ class TonePair:
             if api_result.status_code != 200:
                 print("The forvo API can't be reached, "
                       "terminating the program.")
+                self.connection.close()
                 sys.exit(1)
             # check that the word actually exists
             if len(api_result.json()["items"]) == 1:
@@ -78,49 +82,107 @@ class TonePair:
         # play the audio
         audio_file = vlc.MediaPlayer(self.audio_path)
         audio_file.play()
-        time.sleep(5)
 
     def show_pinyin(self):
         output_string = self.all_data[2] + " " + self.all_data[3]
         print(output_string)
 
-    def get_audio_path(self):
-        return self.audio_path
+    def evaluate_userinput(self):
+        input_string = str(input())
+
+        # parse the input and extract the possible tones
+        if (len(input_string) == 2):
+            first_tone = input_string[0]
+            second_tone = input_string[1]
+            try:
+                first_tone_int = int(first_tone)
+            except:
+                first_tone_int = -1
+
+            try:
+                second_tone_int = int(second_tone)
+            except:
+                second_tone_int = -1
+        else:
+            first_tone_int = -1
+            second_tone_int = -1
+
+        if input_string == "q":
+            self.connection.close()
+            sys.exit(0)
+        elif input_string == "r":
+            self.play_audio()
+            return True
+        elif (first_tone_int > 0 and first_tone_int < 6 and
+              second_tone_int > 0 and second_tone_int < 6):
+            self.check_tones(first_tone_int, second_tone_int)
+            return False
+        else:
+            print("please either enter 'q', 'r' or a tone combination, "
+                  "e.g. 14")
+            return True
+
+    def check_tones(self, first_tone, second_tone):
+        correct_first_tone = int(self.all_data[4])
+        correct_second_tone = int(self.all_data[5])
+        if (first_tone == correct_first_tone and
+                second_tone == correct_second_tone):
+            print("your guess was correct")
+        else:
+            print("your guess was not correct; the correct tones are " +
+                  self.all_data[4] + " " + self.all_data[5])
+        print("the queried word was " + self.all_data[1])
+
+    def update_db(self):
+        new_number_tested = int(self.all_data[7]) + 1
+        new_number_tested_str = str(new_number_tested)
+        self.cursor.execute(
+            "UPDATE toneinfo SET number_tested = ? "
+            "WHERE simplified = ?",
+            (new_number_tested_str, self.all_data[1])
+        )
+        self.connection.commit()
 
 
 def run_app():
     while True:
-        print("p")
         current_pair = TonePair()
         # 1. get a new tonepair
         current_pair.new()
         # 2. play the audio/show pinyin
         current_pair.show_pinyin()
         current_pair.play_audio()
-        # 3. get user input
-        # 4. play again or evaluate
-        # 5. save changes
+        # 3. evaluate user input
+        current_pair_active = True
+        while current_pair_active:
+            current_pair_active = current_pair.evaluate_userinput()
+        # 4. save changes
+        current_pair.update_db()
+        # 5. wait until enter
+        input()
 
 if __name__ == "__main__":
-    test = TonePair()
-    test.new()
-    test.show_pinyin()
-    test.play_audio()
-    # # show startup message
-    # print("Welcome to the (mandarin) tonetrainer (c) 2020 Jonas Hagenberg")
-    # print("Characters and pinyin by CEDICT, pronounciation by forvo.com")
-    # print("For help press 'h + Enter', to start press 's + Enter'")
-    # # start program or show help
-    # input_string = str(input())
-    # while input_string != "h" and input_string != "s" and input_string != "q":
-    #     print("Please enter a character out of the choices")
-    #     input_string = str(input())
-    #
-    # if input_string == "h":
-    #     print("help here")
-    #     # TODO: show help
-    # elif input_string == "s":
-    #     print("start program here")
-    #     run_app()
-    # else:
-    #     sys.exit(0)
+    # test = TonePair()
+    # test.new()
+    # test.show_pinyin()
+    # test.play_audio()
+    # test.evaluate_userinput()
+    # test.update_db()
+    # show startup message
+    print("Welcome to the (mandarin) tonetrainer (c) 2020 - 2021 Jonas Hagenberg")
+    print("Characters and pinyin by CEDICT, pronounciation by forvo.com")
+    print("For help press 'h + Enter', to start press 's + Enter'")
+    # start program or show help
+    input_string = str(input())
+    while input_string != "h" and input_string != "s" and input_string != "q":
+        print("Please enter a character out of the choices")
+        input_string = str(input())
+
+    if input_string == "h":
+        print("help here")
+        # TODO: show help
+    elif input_string == "s":
+        print("start program here")
+        run_app()
+    else:
+        sys.exit(0)
